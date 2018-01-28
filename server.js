6c91c465220705cf7acbf87dc517879da1c3b6d9
Vincent Eberle <veberle@melti.de>
2018-01-28 17:36:35 +0100

    angular routing

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const forceSSL = function () {
   return function (req, res, next) {
@@ -16,6 +17,13 @@ app.use(forceSSL());
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
